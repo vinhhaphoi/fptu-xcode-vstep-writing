223b10460f7a8376c9b6f1177b2c9c513bb86f8d
vinhhaphoi <vinhhaphoi@users.noreply.github.com>
2026-02-05 21:46:11 +0700

    Initial Commit

---
+++ VSTEP Writing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VSTEP Writing
+//
+//  Created by Vinh Hap Hoi on 2/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VSTEP Writing/VSTEP_WritingApp.swift
@@ -0,0 +1,17 @@
+//
+//  VSTEP_WritingApp.swift
+//  VSTEP Writing
+//
+//  Created by Vinh Hap Hoi on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct VSTEP_WritingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
